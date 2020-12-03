855b97d8cc1ddc33621bd70a47bade2fad27034a
I-Infinatoo <ishurajan234@gmail,com>
2020-12-03 17:03:54 +0530

    First application using Kotlin

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,19 +27,51 @@ class MainActivity : AppCompatActivity() {
 
         /**
          * call Dice class with '6'
-         * and pass the returned value to the textView widget
          **/
-        val dice = Dice(6)
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = dice.roll().toString()
 
+        val dice = Dice(6)
+
+        /**
+         * Update the imageView with the random number generated
+         * And set the content description for screen reader
+         **/
+
+        val diceImage : ImageView  = findViewById(R.id.imageView)
+        val diceImage2 : ImageView = findViewById(R.id.imageView2)
+
+        val num  = dice.roll()
+        val num2 = dice.roll()
+
+        val image = when ( num ) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val image2 = when ( num2 ) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+        diceImage.setImageResource(image)
+        diceImage.contentDescription = num.toString()
+
+        diceImage2.setImageResource(image2)
+        diceImage2.contentDescription = num2.toString()
     }
 }
 
 class Dice(private val numSide: Int) {
 
     /**
-     * roll will return the random number from [1,6] and return it
+     * roll will return the random number from [1,6]
      **/
     fun roll(): Int {
         return (1..numSide).random()
